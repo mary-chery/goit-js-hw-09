d7a446c099190c128c65a63cf78b0e88aeb11cf5
mary-chery <109818312+mary-chery@users.noreply.github.com>
2023-03-02 11:51:14 +0000

    Deploying to gh-pages from @ mary-chery/goit-js-hw-09@94d6d580c6f32de1f1c1d009c8088a505fa0edd6 🚀

---
+++ 01-color-switcher.3f49bcd0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");t.disabled=!0;let a=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.3f49bcd0.js.map
